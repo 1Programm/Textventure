366013e23d29806a38fb32264f974e2d4c57df70
Julian Becker <J.Becker@geniusbytes.com>
2019-04-10 19:25:17 +0200

    Init Commit for Project

---
+++ src/main/java/com/programm/game/Textventure/App.java
@@ -0,0 +1,13 @@
+package com.programm.game.Textventure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
